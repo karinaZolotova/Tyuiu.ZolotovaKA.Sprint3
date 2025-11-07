06439a5e0f2654b6631359fb30825ab4db941d78
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-07 18:10:56 +0500

    Task2

--- Tyuiu.ZolotovaKA.Sprint3.Task2.V7.Lib/DataService.cs
+++ Tyuiu.ZolotovaKA.Sprint3.Task2.V7.Lib/DataService.cs
@@ -6,8 +6,13 @@ namespace Tyuiu.ZolotovaKA.Sprint3.Task2.V7.Lib
     {
         public double GetSumSeries(int startValue, int stopValue)
         {
-
-            return 1;
+            double sumSeries = 0;
+            do
+            {
+                sumSeries = sumSeries + (Math.Sin(startValue) * (Math.Pow((0.5), 2)));
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(sumSeries, 3);
         }
     }
 }
