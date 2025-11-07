3ad1492ebb9b3de44740ee517bf11559072e9a7b
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-07 18:03:55 +0500

    Task1

---
+++ Tyuiu.ZolotovaKA.Sprint3.Task1.V15.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.ZolotovaKA.Sprint3.Task1.V15.Lib;
+namespace Tyuiu.ZolotovaKA.Sprint3.Task1.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            int value = 2;
+            int startValue = 1;
+            int stopValue = 5;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 208845;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
