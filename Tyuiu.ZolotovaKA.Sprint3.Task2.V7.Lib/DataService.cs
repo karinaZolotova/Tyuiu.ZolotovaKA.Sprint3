213db890e3edc5109ad2ee23bcad28b5b703a1cb
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-07 18:09:54 +0500

    Task2

--- Tyuiu.ZolotovaKA.Sprint3.Task2.V7.Lib/DataService.cs
+++ Tyuiu.ZolotovaKA.Sprint3.Task2.V7.Lib/DataService.cs
@@ -6,21 +6,8 @@ namespace Tyuiu.ZolotovaKA.Sprint3.Task2.V7.Lib
     {
         public double GetSumSeries(int startValue, int stopValue)
         {
-            throw new NotImplementedException();
-        }
 
-        public string ReplaceCharOnNum(string value, char replaceable, char replacement)
-        {
-            string res = "";
-            foreach (char chr in value)
-            {
-                if (chr == replaceable)
-                {
-                    value = value.Replace(chr, replacement);
-                    res = value;
-                }
-            }
-            return res;
+            return 1;
         }
     }
 }
